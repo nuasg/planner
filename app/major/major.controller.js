5b736e56c5f106405157db13deab34e6f93b8020
Philip Lan <philiplan2017@u.northwestern.edu>
2016-01-15 14:39:14 -0600

    started workingon major view

--- app/major/major.controller.js
+++ app/major/major.controller.js
@@ -1,6 +1,14 @@
 (function(){
 	angular.module("prereqsmap")
 	.controller("MajorController",["$scope","$state",function($scope,$state){
-		
+		$scope.addMajor = function (major, classes) {
+			if (classes == undefined || major == undefined) {
+				alert("Fill out form completely");
+			} else {
+				$scope.major = major;
+				$scope.listOfClasses = classes.split(",");
+				$('#classes').modal('show');
+			}
+		}
 	}]);
 }());
