bd27069cf7b27f97657ddd9123f193dbc9861cf9
Philip Lan <philiplan2017@u.northwestern.edu>
2016-01-17 21:17:34 -0600

    setting up options to add majors

--- app/major/major.controller.js
+++ app/major/major.controller.js
@@ -1,6 +1,10 @@
 (function(){
 	angular.module("prereqsmap")
-	.controller("MajorController",["$scope","$state",function($scope,$state){
+	.controller("MajorController",["$scope","$state","$http",function($scope,$state,$http){
+		$http.get("/api/school").success(function(data){
+			$scope.schools = data;
+		})
+
 		$scope.addMajor = function (major, classes) {
 			if (classes == undefined || major == undefined) {
 				alert("Fill out form completely");
@@ -9,6 +13,19 @@
 				$scope.listOfClasses = classes.split(",");
 				$('#classes').modal('show');
 			}
+		}
+		$scope.deleteConfirmation = function(school, major){
+			$scope.selectedMajor = major;
+			$scope.selectedSchool = school;
+			$('#delete').modal('show');
+		}
+		$scope.deleteMajor = function (school, major) {
+			var index = school.majors.indexOf(major);
+			if (index >= 0) {
+				school.majors.splice( index, 1 );
+			}
+			$scope.school = school;
+			$('#delete').modal('hide');
 		}
 	}]);
 }());
