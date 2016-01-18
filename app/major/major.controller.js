260dc268dcc60308505f5a718dfdad84220e30d3
kevincheng96 <kevincheng96@hotmail.com>
2016-01-18 17:21:01 -0600

    added  add major request

--- app/major/major.controller.js
+++ app/major/major.controller.js
@@ -1,6 +1,6 @@
 (function(){
 	angular.module("prereqsmap")
-	.controller("MajorController",["$scope","$state",function($scope,$state){
+	.controller("MajorController",["$scope","$state","$http",function($scope,$state,$http){
 		$scope.addMajor = function (major, classes) {
 			if (classes == undefined || major == undefined) {
 				alert("Fill out form completely");
@@ -8,6 +8,15 @@
 				$scope.major = major;
 				$scope.listOfClasses = classes.split(",");
 				$('#classes').modal('show');
+				var data = {
+					name: $scope.major, 
+					classes: $scope.listOfClasses
+				};
+				$http.post("api/major", data).success(function(response){
+
+				}).error(function(error){
+					console.log(error);
+				});
 			}
 		}
 	}]);
