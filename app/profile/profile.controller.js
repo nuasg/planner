556cff9030746f867975cda26da2b92b88e9fcaa
kevincheng96 <kevincheng96@hotmail.com>
2016-05-22 17:06:05 -0500

    added graph into graph.controller.js

--- app/profile/profile.controller.js
+++
@@ -1,9 +0,0 @@
-(function(){
-	angular.module("prereqsmap")
-	.controller("ProfileController",["$scope", "$state", "$http", "$cookies", "DataFactory", function($scope, $state, $http, $cookies, DataFactory){
-		$scope.query = DataFactory.query.school ? DataFactory.query : $cookies.getObject("query");
-		if (!$scope.query) {
-			$state.go("home");
-		}
-	}]);
-}());
--- app/profile/sidebar.controller.js
+++
@@ -1,9 +0,0 @@
-(function(){
-	angular.module("prereqsmap")
-	.controller("SidebarController",["$scope", "$state", "$http", "$cookies", "DataFactory", function($scope, $state, $http, $cookies, DataFactory){		
-		$scope.query = DataFactory.query.school ? DataFactory.query : $cookies.getObject("query");
-		if (!$scope.query) {
-			$state.go("home");
-		}
-	}]);
-}());
